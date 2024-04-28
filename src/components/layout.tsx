9ff4110f8001bd2062b3aa729f149dc17f614fa8
Paul Lee <swaniris@gmail.com>
2024-04-28 17:43:20 +0900

    feat: #1 Use tailwind layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,25 +9,20 @@ type Props = PropsWithChildren<{
 const Layout = ({ location, title, children }: Props) => {
   const rootPath = `${__PATH_PREFIX__}/`;
   const isRootPath = location?.pathname === rootPath;
-  let header;
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    );
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    );
-  }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <div className="container mx-auto my-8" data-is-root-path={isRootPath}>
+      <header className="global-header">
+        {isRootPath ? (
+          <h1 className="text-6xl">
+            <Link to="/">{title}</Link>
+          </h1>
+        ) : (
+          <Link className="text-3xl" to="/">
+            {title}
+          </Link>
+        )}
+      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
